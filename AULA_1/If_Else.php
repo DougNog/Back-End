3416ff936ba5717d1bfec45af8c468aceaaa32a1
DougNog <douglasnogueirapjunior@gmail.com>
2025-08-08 13:46:26 -0300

    TDS

--- AULA_1/If_Else.php
+++ AULA_1/If_Else.php
@@ -1,28 +1,26 @@
 <?php
 
-    $nota1 = 0;
-    $nota2 = 0;
-    $aulas_Totais = 100;
-    $aulas_Presentes = 70;
-    $nome = "Enzo Enrico";
+$nota1 = 0;
+$nota2 = 0;
+$aulas_Totais = 100;
+$aulas_Presentes = 70;
+$nome = "Enzo Enrico";
 
-    // --- Cálculos --- //
-    $media = ($nota1 + $nota2) / 2;
-    $frequencia = ($aulas_Presentes / $aulas_Totais) * 100;
+// --- Cálculos --- //
+$media = ($nota1 + $nota2) / 2;
+$frequencia = ($aulas_Presentes / $aulas_Totais) * 100;
 
-    // --- Verificação da Condição --- //
-    // Primeiro, verificamos se o aluno atende a AMBOS os critérios para ser aprovado.
-    if ($media >= 7 && $frequencia >= 70 || $nome == "Enzo Enrico") {
-        echo "Aprovado!";
-    } else {
-        //motivo da reprovação.
-        if ($media < 7 && $frequencia < 70) {
-            echo "Reprovado por média e frequência!";
-        } else if ($media < 7) {
-            echo "Reprovado por média!";
-        } else { // A única condição restante é a reprovação por frequência.
-            echo "Reprovado por frequência!";
-        }
+// --- Verificação da Condição --- //
+// Primeiro, verificamos se o aluno atende a AMBOS os critérios para ser aprovado.
+if ($media >= 7 && $frequencia >= 70 || $nome == "Enzo Enrico") {
+    echo "Aprovado!";
+} else {
+    //motivo da reprovação.
+    if ($media < 7 && $frequencia < 70) {
+        echo "Reprovado por média e frequência!";
+    } else if ($media < 7) {
+        echo "Reprovado por média!";
+    } else { // A única condição restante é a reprovação por frequência.
+        echo "Reprovado por frequência!";
     }
-
-?>
+}
