84481eb9e911b10dafda616aa16767175109cc5f
Douglas <douglasnogueirapjunior@gmail.com>
2025-08-20 15:07:15 -0300

    20/08/2025

---
+++ AULA_5/Atividade_POO.php
@@ -0,0 +1,40 @@
+<?php
+
+//!Crie uma classe (molde de objetos) chamada 'Cachorro' com os seguintes atributos: Nome, idade, raca, castrado e sexo
+//! após a criação da classe crie 10 objetos
+
+class Cachorro
+{
+    public $nome;
+
+    public $idade;
+
+    public $raca;
+
+    public $castrado;
+
+    public $sexo;
+
+    public function __construct($nome, $idade, $raca, $castrado, $sexo)
+    {
+
+        $this->nome = $nome;
+        $this->idade = $idade;
+        $this->raca = $raca;
+        $this->castrado = $castrado;
+        $this->sexo = $sexo;
+    }
+}
+
+$cachorro1 = new Cachorro("Rex", 5, "Labrador", true, "Macho");
+$cachorro2 = new Cachorro("Bella", 3, "Poodle", false, "Fêmea");
+$cachorro3 = new Cachorro("Max", 2, "Bulldog", true, "Macho");
+$cachorro4 = new Cachorro("Luna", 4, "Beagle", false, "Fêmea");
+$cachorro5 = new Cachorro("Charlie", 1, "Golden Retriever", true, "Macho");
+$cachorro6 = new Cachorro("Molly", 6, "Shih Tzu", false, "Fêmea");
+$cachorro7 = new Cachorro("Buddy", 3, "Cocker Spaniel", true, "Macho");
+$cachorro8 = new Cachorro("Daisy", 2, "Dachshund", false, "Fêmea");
+$cachorro9 = new Cachorro("Rocky", 5, "Boxer", true, "Macho");
+$cachorro10 = new Cachorro("Sadie", 4, "Schnauzer", false, "Fêmea");
+
+?>
--- AULA_5/Utilizando_POO.php
+++ AULA_5/Utilizando_POO.php
@@ -28,6 +28,8 @@ $carro1 = new Carro("Porsche" , "911" , "2020" , false , 3);
 $carro2 = new Carro("Mitsubishi" , "Lancer" , "1945" , true , 1);
 $carro3 = new Carro("Chevrolet" , "Onix" , "2022" , false , 0);
 $carro4 = new Carro("Volkswagen" , "Fusca" , "1970" , true , 5);
+$carro5 = new Carro("Fiat" , "Uno" , "1990" , false , 2);
+$carro6 = new Carro("Ford" , "Ka" , "2018" , true , 4);
 
 
 
