d6532067d5f8629506f6e568ce2b9c58ad0abac6
Douglas <douglasnogueirapjunior@gmail.com>
2025-11-07 13:24:19 -0300

    .

--- AULA_13/index.php
+++ AULA_13/index.php
@@ -9,21 +9,21 @@
     // Determine os precos por conta e os codigos de forma aleatoria.
 
     // Create
-$dao->criarProdutos(new Produtos(1, "Tomate", 8.82));
+$dao->criarProdutos(new Produtos(1, "Tomate", 5.00));
 
 $dao->criarProdutos(new Produtos(2, "Maçã", 2.86));
 
-$dao->criarProdutos(new Produtos(3, "Queijo Brie", 29.90));
+$dao->criarProdutos(new Produtos(3, "Queijo Brie", 20.90));
 
-$dao->criarProdutos(new Produtos(4, "Iogurte Grego", 7.50));
+$dao->criarProdutos(new Produtos(4, "Iogurte Grego", 6.99));
 
-$dao->criarProdutos(new Produtos(5, "Guaraná Jesus", 6.25));
+$dao->criarProdutos(new Produtos(5, "Guaraná Jesus", 8.20));
 
-$dao->criarProdutos(new Produtos(6, "Bolacha Bono", 4.35));
+$dao->criarProdutos(new Produtos(6, "Bolacha Bono", 5.35));
 
-$dao->criarProdutos(new Produtos(7, "Desinfetante Urca", 5.60));
+$dao->criarProdutos(new Produtos(7, "Desinfetante Urca", 7.99));
 
-$dao->criarProdutos(new Produtos(8, "Prestobarba Bic", 12.90));
+$dao->criarProdutos(new Produtos(8, "Prestobarba Bic", 14.90));
 
     
 
